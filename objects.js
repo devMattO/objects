eb8d0508f71c3e0ce603c38bd1208a7ac9b01142
Mattdre <olsen_matthew@yahoo.com>
2016-03-10 20:37:53 -1000

    buildPerson

--- objects.js
+++ objects.js
@@ -15,3 +15,19 @@ var stockCar = {
 	driver: null,
 	passengers: [],
 };
+
+// #3
+
+var plainPerson = {};
+
+function buildPerson(person,nameString,age) {
+	person.name = nameString;
+	person.age = age;
+	return person;
+}
+
+var builtPerson = buildPerson(plainPerson,'matt',22);
+console.log(builtPerson);
+
+console.log(plainPerson.name);
+console.log(plainPerson.age);
