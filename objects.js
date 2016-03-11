fcfabace657dd865df48d9f910e49d6dbbf5d2b9
Mattdre <olsen_matthew@yahoo.com>
2016-03-10 20:47:01 -1000

    display values of objects in an array

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-// #1
+// # 1
 
 var plainBox = {
 	color: 'green',
@@ -6,7 +6,7 @@ var plainBox = {
 	contents: []
 };
 
-// #2
+// # 2
 
 var stockCar = {
 	model: 'Ford',
@@ -16,7 +16,7 @@ var stockCar = {
 	passengers: [],
 };
 
-// #3
+// # 3
 
 var plainPerson = {};
 
@@ -31,3 +31,65 @@ console.log(builtPerson);
 
 console.log(plainPerson.name);
 console.log(plainPerson.age);
+
+// # 4
+
+var arrayOfObjects = [
+  {
+    id: 0,
+    date: "Monday Jan 25 2015 2:01 PM",
+    total: "279.38"
+  },
+  {
+    id: 1,
+    date: "Monday Jan 27 2015 11:31 AM",
+    total: "79.80"
+  },
+  {
+    id: 2,
+    date: "Monday Feb 1 2015 7:56 AM",
+    total: "15.62"
+  },
+  {
+    id: 3,
+    date: "Monday Feb 1 2015 9:43 AM",
+    total: "19.83"
+  },
+  {
+    id: 4,
+    date: "Monday Feb 1 2015 11:08 PM",
+    total: "56.69"
+  },
+  {
+    id: 5,
+    date: "Monday Feb 13 2015 10:22 AM",
+    total: "137.92"
+  },
+  {
+    id: 6,
+    date: "Monday Feb 14 2015 6:54 PM",
+    total: "938.65"
+  },
+  {
+    id: 7,
+    date: "Monday Feb 14 2015 7:17 PM",
+    total: "43.77"
+  },
+  {
+    id: 8,
+    date: "Monday Feb 14 2015 7:18 PM",
+    total: "28.54"
+  },
+  {
+    id: 9,
+    date: "Monday Feb 14 2015 7:18 PM",
+    total: "194.33"
+  }
+];
+
+function printProcessedOrders(orders) {
+	for (var i = 0; i <= 2; i++) {
+		console.log(arrayOfObjects[i]);
+	}
+}
+printProcessedOrders(arrayOfObjects);
